3061afc4791b91fe0746ec252c01b13d36aefef5
1amKhush <khushvendras99@gmail.com>
2025-06-19 00:03:54 +0530

    Question mentioned before each code snippet

--- Khushvendra/practice.go
+++ Khushvendra/practice.go
@@ -1,9 +1,9 @@
 // Arrays & Counting : 
-//Sum of Elements - Calculate and print the sum of all numbers in an array.
 
-// Count Even Numbers - Count how many even numbers are present in the array.
 
-// Count Odd Numbers - Count how many odd numbers are present in the array.
+
+
+
 
 package main
 
@@ -11,6 +11,8 @@ import "fmt"
 
 func main1() {
 	var arr [10]int = [10]int{1,2,3,4,5,6,7,8,9,10}
+
+	//Sum of Elements - Calculate and print the sum of all numbers in an array.
 	sum := 0
 
 	for i:=0; i<10; i++ {
@@ -18,6 +20,7 @@ func main1() {
 	}
 	fmt.Println("sum of array :",sum)
 
+	// Count Even Numbers - Count how many even numbers are present in the array.
 	isEven := 0
 
 	for j:=0; j<10; j++ {
@@ -27,6 +30,7 @@ func main1() {
 	}
 	fmt.Println("even elements :", isEven)
 
+	// Count Odd Numbers - Count how many odd numbers are present in the array.
 	isOdd := 0
 	for k:=0; k<10; k++ {
 		if arr[k] % 2 == 0 {
